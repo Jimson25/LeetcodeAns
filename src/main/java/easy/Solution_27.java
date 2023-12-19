81dbb8d8c181749c96561a424add66ed03279a4d
Jimson25 <248578737@qq.com>
2023-12-19 11:17:56 +0800

    solution 27

---
+++ src/main/java/easy/Solution_27.java
@@ -0,0 +1,46 @@
+package easy;
+
+import java.util.Arrays;
+
+class Solution_27 {
+    public int removeElement(int[] nums, int val) {
+        int eqIndex = 0, noEqIndex = 0;
+
+        while (noEqIndex < nums.length && eqIndex < nums.length) {
+            if (nums[eqIndex] != val) {
+                eqIndex++;
+                continue;
+            }
+
+            if (noEqIndex <= eqIndex) {
+                noEqIndex = eqIndex + 1;
+                continue;
+            }
+
+            if (nums[noEqIndex] == val) {
+                noEqIndex++;
+                continue;
+            }
+
+            int t = nums[eqIndex];
+            nums[eqIndex] = nums[noEqIndex];
+            nums[noEqIndex] = t;
+            noEqIndex++;
+            eqIndex++;
+
+        }
+        return eqIndex;
+    }
+
+    public static void main(String[] args) {
+        Solution_27 s = new Solution_27();
+        int[] nums = {2};
+        int val = 3;
+
+
+        System.out.println(s.removeElement(nums, val));
+        System.out.println(Arrays.toString(nums));
+
+
+    }
+}
