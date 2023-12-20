40f5a9f3cc2c545075cbea99058e4cf0705b2a9c
Jimson25 <248578737@qq.com>
2023-12-20 09:38:12 +0800

    Solution 35

---
+++ src/main/java/easy/Solution_35.java
@@ -0,0 +1,29 @@
+package easy;
+
+public class Solution_35 {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]>=target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        // case 1
+//         int[] nums = {1, 3, 5, 6};
+//         int target = 5;
+
+        // case 2
+         int[] nums = {1, 3, 5, 6};
+         int target = 2;
+
+        // case 3
+//        int[] nums = {1, 3, 5, 6};
+//        int target = 0;
+
+        Solution_35 s = new Solution_35();
+        System.out.println(s.searchInsert(nums, target));
+    }
+}
